072804f8f4c1808647bc6f9738d820ac1e4a2b7e
Liuchenling <wssgcg1213@qq.com>
2015-03-15 16:28:27 +0800

    fix

--- addTeamInfo.php
+++ addTeamInfo.php
@@ -15,22 +15,22 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>拉拉队添加</title>
+		<title>投票条目添加</title>
 		<link rel="stylesheet" href="./css/add.css"/>
 	</head>
 	<body>
 		<div id="content">
-			<h2>队伍添加</h2>
+			<h2>添加</h2>
 			<form action="" method="post" name="addTeamForm">
 			<dl>
-				<dd>队 伍 名： <input type="text" name="teamName" class="text" /></dd>
+				<dd>条目名： <input type="text" name="teamName" class="text" /></dd>
 
-				<dd>头像选择： <input type="button" class="text face" value="点击选择" onclick="javascript:window.open('uploadImage/selectImage.php','face','width=600,height=600,top=200,left=200')" /></dd>
+				<dd>图片选择： <input type="button" class="text face" value="点击选择" onclick="javascript:window.open('uploadImage/selectImage.php','face','width=600,height=600,top=200,left=200')" /></dd>
 				<dd>
-					头像显示： <img src="" style="width:100px;height:100px;" id="faceId" alt="队伍头像" />
+					图片显示： <img src="" style="width:100px;height:100px;" id="faceId" alt="图片" />
 					<input type="hidden" name="teamFaceUrl" id="faceInputId" />
 				</dd>
-				<dd>微视链接： <input type="text" name="smallVideoUrl" class="text" /></dd>
+<!--				<dd>video链接： <input type="text" name="smallVideoUrl" class="text" /></dd>-->
 				<dd>详情链接： <input type="text" name="roo_detail_href" class="text" /></dd>
 				<dd><span style="vertical-align:190px">描　　述：</span> <textarea name="teamDesc"></textarea></dd>
 				
@@ -57,17 +57,17 @@
 			function check()
 			{
 				if (addTeamForm.teamName.value==""){
-					alert("请输入队伍名！");
+					alert("请输入条目名！");
 					return false;
 				}
 				if (addTeamForm.teamFaceUrl.value==""){
-					alert("请选择队伍头像！");
+					alert("请选择图片！");
 					return false;
 				}
-				if (addTeamForm.smallVideoUrl.value==""){
-					alert("请输入微视频链接!");
-					return false;
-				}
+//				if (addTeamForm.smallVideoUrl.value==""){
+//					alert("请输入视频链接!");
+//					return false;
+//				}
 				if (addTeamForm.roo_detail_href.value==""){
 					alert("请输入详情链接!");
 					return false;
@@ -76,10 +76,10 @@
 					alert("请输入描述！");
 					return false;
 				}
-				if(!checkUrl(addTeamForm.smallVideoUrl.value)){
-					alert('微视频链接格式不正确');
-					return false;
-				}
+//				if(!checkUrl(addTeamForm.smallVideoUrl.value)){
+//					alert('微视频链接格式不正确');
+//					return false;
+//				}
 				if(!checkUrl(addTeamForm.roo_detail_href.value)){
 					alert('详情链接格式不正确');
 					return false;
